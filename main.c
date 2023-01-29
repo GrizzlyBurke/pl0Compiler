44572ce799649db407be20a6e3ffa24b753b06f8
Mike Burke <michaelburke.eh@gmail.com>
2023-01-28 19:56:46 -0500

    Formatting update, case work

--- main.c
+++ main.c
@@ -9,15 +9,16 @@ typedef struct instruction
 }instruction;
 
 void initStack(int * stack);
-void printInstructions(instruction * code, int count);
-void printPointers( int BP, int SP, int PC);
-void printStack(int * stack, int SC);
+void printCode(instruction * code, int count);
+void printPointers( int BP, int SP, int PC,int NDB);
+void printStack(int * stack, int SP, int NDB);
+void printInstruction(int PC, int OP, int M, int NDB);
 
 int main(int argc, char *argv[]) 
 {
     instruction code[MAX_CODE_LENGTH];
     int stack[MAX_STACK_HEIGHT];
-    int BP = 0, SP = 0, PC = 0;
+    int BP = 0, SP = 0, PC = 0, NDB = 0;
 
     //initilize the stack
     initStack(stack);
@@ -44,14 +45,14 @@ int main(int argc, char *argv[])
     }
     fclose(fp);
 
-    printInstructions(code, i);
+    printCode(code, i);
     printf("Tracing...\n");
-    printPointers(BP, SP, PC);
-    printStack(stack, SP);
+    printPointers(BP, SP, PC, NDB);
+    
 
     while (1) 
     {
-        
+        printStack(stack, SP, NDB);
 
             if (SP >= MAX_STACK_HEIGHT) {
                 printf("Error: stack overflow\n");
@@ -64,164 +65,186 @@ int main(int argc, char *argv[])
 
             switch (code[PC].opcode) {
             case 1: // LIT
-                printf("==> addr: %d\t LIT\t %d\n", PC, code[PC].m); 
+                printInstruction(PC, code[PC].opcode, code[PC].m, NDB);
                 stack[SP] = code[PC].m;
                 SP++;
                 PC++;
                 break;
             case 2: // RTN
-                printf("==> addr: %d\t RTN\t %d\n", PC, code[PC].m); 
+                printInstruction(PC, code[PC].opcode, code[PC].m, NDB);                
                 SP = BP - 1;
                 PC = stack[SP + 3];
                 BP = stack[SP + 2];
                 break;
             case 3: // CAL
-                printf("==> addr: %d\t CAL\t %d\n", PC, code[PC].m); 
+                printInstruction(PC, code[PC].opcode, code[PC].m, NDB); 
                 stack[SP + 1] = BP;
                 stack[SP + 2] = PC;
                 BP = SP + 2;
                 PC = code[PC + 1].m;
                 break;
             case 4: // POP
-                printf("==> addr: %d\t POP\t %d\n", PC, code[PC].m); 
+                printInstruction(PC, code[PC].opcode, code[PC].m, NDB); 
                 SP--;
                 break;
             case 5: // PSI
-                printf("==> addr: %d\t PSI\t %d\n", PC, code[PC].m); 
-                stack[SP] = stack[stack[SP]];
+                printInstruction(PC, code[PC].opcode, code[PC].m, NDB);; 
+                stack[SP - 1] = stack[SP-2];
+                PC++;
                 break;
             case 6: // PRM
-                printf("==> addr: %d\t PRM\t %d\n", PC, code[PC].m); 
+                printInstruction(PC, code[PC].opcode, code[PC].m, NDB); 
                 SP++;
                 stack[SP] = stack[BP - code[PC + 1].m];
                 PC++;
                 break;
             case 7: // STO
-                printf("==> addr: %d\t STO\t %d\n", PC, code[PC].m); 
+                printInstruction(PC, code[PC].opcode, code[PC].m, NDB); 
                 stack[stack[SP - 1] + code[PC + 1].m] = stack[SP - 2];
                 SP -= 2;
                 PC++;
                 break;
             case 8: // INC
-                printf("==> addr: %d\t INC\t %d\n", PC, code[PC].m); 
+                printInstruction(PC, code[PC].opcode, code[PC].m, NDB); 
                 SP += code[PC].m;
                 PC++;
                 break;
             case 9: // JMP
-                printf("==> addr: %d\t JMP\t %d\n", PC, code[PC].m); 
+                printInstruction(PC, code[PC].opcode, code[PC].m, NDB); 
                 PC = stack[SP - 1];
                 SP--;
                 break;
             case 10: // JPC
-                printf("==> addr: %d\t JPC\t %d\n", PC, code[PC].m); 
+                printInstruction(PC, code[PC].opcode, code[PC].m, NDB); 
                 if (stack[SP - 1] != 0) {
-                    PC = code[PC + 1].m;
+                    PC = code[PC].m;
                 }
+                else
+                    PC++;
+                    SP--;
+                break;
+            case 11: // CHO
+                printInstruction(PC, code[PC].opcode, code[PC].m, NDB); 
+                fprintf(stderr, "%c", stack[SP - 1]);
                 SP--;
                 PC++;
                 break;
-            case 11: // CHO
-                printf("==> addr: %d\t CHO\t %d\n", PC, code[PC].m); 
-                printf("%c", stack[SP - 1]);
-                SP--;
-                break;
             case 12: // CHI
-                printf("==> addr: %d\t CHI\t %d\n", PC, code[PC].m); 
-                SP++;
+                printInstruction(PC, code[PC].opcode, code[PC].m, NDB); 
                 stack[SP] = getchar();
+                SP++;
+                PC++;
                 break;
             case 13: //HLT
                 PC++;
                 break;
             case 14: // NDB
-                printf("==> addr: %d\t NBD\t %d\n", PC, code[PC].m); 
-                // stop printing debugging output
+                // sets teh NBD variable to 1 to prevent debug printing
+                printInstruction(PC, code[PC].opcode, code[PC].m, NDB); 
+                NDB = 1;
+                PC++;
+                
                 break;
             case 15: // NEG
-                printf("==> addr: %d\t NEG\t %d\n", PC, code[PC].m); 
+                printInstruction(PC, code[PC].opcode, code[PC].m, NDB); 
                 stack[SP - 1] = -stack[SP - 1];
+                PC++;
                 break;
             case 16: // ADD
-                printf("==> addr: %d\t ADD\t %d\n", PC, code[PC].m); 
+                printInstruction(PC, code[PC].opcode, code[PC].m, NDB); 
                 stack[SP - 2] = stack[SP - 1] + stack[SP - 2];
                 SP -= 1;
+                PC++;
                 break;
             case 17: // SUB
-                printf("==> addr: %d\t SUB\t %d\n", PC, code[PC].m); 
+                printInstruction(PC, code[PC].opcode, code[PC].m, NDB); 
                 stack[SP - 2] = stack[SP - 1] - stack[SP - 2];
                 SP -= 1;
+                PC++;
                 break;
             case 18: // MUL
-                printf("==> addr: %d\t MUL\t %d\n", PC, code[PC].m); 
+                printInstruction(PC, code[PC].opcode, code[PC].m, NDB); 
                 stack[SP - 2] = stack[SP - 1] * stack[SP - 2];
                 SP -= 1;
+                PC++;
                 break;
             case 19:  // DIV
-                printf("==> addr: %d\t DIV\t %d\n", PC, code[PC].m); 
+                printInstruction(PC, code[PC].opcode, code[PC].m, NDB); 
                 if(stack[SP-2] == 0){
                     fprintf(stderr, "Error: Division by zero\n");
                     exit(EXIT_FAILURE);
                 }
                 stack[SP-2] = stack[SP-1] / stack[SP-2];
                 SP--;
+                PC++;
                 break;
             case 20:  // MOD
-                printf("==> addr: %d\t MOD\t %d\n", PC, code[PC].m); 
+                printInstruction(PC, code[PC].opcode, code[PC].m, NDB); 
                 if(stack[SP-2] == 0){
                     fprintf(stderr, "Error: Division by zero\n");
                     exit(EXIT_FAILURE);
                 }
                 stack[SP-2] = stack[SP-1] % stack[SP-2];
                 SP--;
+                PC++;
                 break;
             case 21: // EQL
-                printf("==> addr: %d\t EQL\t %d\n", PC, code[PC].m); 
+                printInstruction(PC, code[PC].opcode, code[PC].m, NDB); 
                 stack[SP - 2] = stack[SP - 1] == stack[SP - 2] ? 1 : 0;
                 SP -= 1;
+                PC++;
                 break;
             case 22: // NEQ
-                printf("==> addr: %d\t NEQ\t %d\n", PC, code[PC].m); 
+                printInstruction(PC, code[PC].opcode, code[PC].m, NDB); 
                 stack[SP - 2] = stack[SP - 1] != stack[SP - 2] ? 1 : 0;
+                stack[SP - 1] = 0;
                 SP -= 1;
+                PC++;
                 break;
             case 23: // LSS
-                printf("==> addr: %d\t LSS\t %d\n", PC, code[PC].m); 
+                printInstruction(PC, code[PC].opcode, code[PC].m, NDB); 
                 stack[SP - 2] = stack[SP - 1] < stack[SP - 2] ? 1 : 0;
                 SP -= 1;
+                PC++;
                 break;
             case 24: // LEQ
-                printf("==> addr: %d\t LEQ\t %d\n", PC, code[PC].m); 
+                printInstruction(PC, code[PC].opcode, code[PC].m, NDB); 
                 stack[SP - 2] = stack[SP - 1] <= stack[SP - 2] ? 1 : 0;
                 SP -= 1;
+                PC++;
                 break;
             case 25: // GTR
-                printf("==> addr: %d\t GTR\t %d\n", PC, code[PC].m); 
+                printInstruction(PC, code[PC].opcode, code[PC].m, NDB); 
                 stack[SP - 2] = stack[SP - 1] > stack[SP - 2] ? 1 : 0;
                 SP -= 1;
+                PC++;
                 break;
             case 26: // GEQ
-                printf("==> addr: %d\t GEQ\t %d\n", PC, code[PC].m); 
+                printInstruction(PC, code[PC].opcode, code[PC].m, NDB); 
                 stack[SP - 2] = stack[SP - 1] >= stack[SP - 2] ? 1 : 0;
                 SP -= 1;
+                PC++;
                 break;
             case 27: // PSP
-                printf("==> addr: %d\t PSP\t %d\n", PC, code[PC].m); 
+                printInstruction(PC, code[PC].opcode, code[PC].m, NDB); 
                 stack[SP] = SP;
                 SP += 1;
+                PC++;
                 break;
             default:
                 printf("Error: Invalid instruction %d at PC = %d\n", code[PC].opcode, PC);
                 return 1;
         }
 
-        printPointers(BP, SP, PC);
-        printStack(stack, SP);
+            printPointers(BP, SP, PC, NDB);
+            
         if(code[PC].opcode == 13)
             {
-                printf("==> addr: %d\t HLT\t %d\n", PC, code[PC].m);
+                printStack(stack, SP, NDB);
+                printInstruction(PC, code[PC].opcode, code[PC].m, NDB);
                 PC++;
-                printPointers(BP, SP, PC);
-                printStack(stack, SP);
+                printPointers(BP, SP, PC, NDB);
+                printStack(stack, SP, NDB);
                 break;
             }
     }
@@ -237,7 +260,7 @@ void initStack(int * stack)
     }
 }
 
-void printInstructions(instruction * code, int count)
+void printCode(instruction * code, int count)
 {
     printf("ADR\t OP\t m\n");
     for (int i = 0; i < count; i++ )
@@ -246,17 +269,115 @@ void printInstructions(instruction * code, int count)
     }
 }
 
-void printPointers(int BP, int SP, int PC)
+void printPointers(int BP, int SP, int PC, int NDB)
 {
-    printf("PC: %d BP: %d SP: %d\n", PC, BP, SP);
+    if( NDB == 0)
+    {
+        printf("PC: %d BP: %d SP: %d\n", PC, BP, SP);
+    }
 }
 
-void printStack(int * stack, int SP)
+void printStack(int * stack, int SP, int NDB)
 {
-    printf("stack: ");
-    for (int i = 0; i < SP; i++)
+    if(NDB == 0)
     {
-        printf("s[%d]: %d ", i, stack[i]);
+        printf("stack: ");
+        for (int i = 0; i < SP; i++)
+        {
+            printf("S[%d]: %d ", i, stack[i]);
+        }
+        printf("\n");
     }
-    printf("\n");
+}
+
+void printInstruction(int PC, int OP, int M, int NDB)
+{
+    if(NDB == 0)
+    {
+        switch(OP)
+        {
+            case 1:
+                printf("==> addr: %d     LIT   %d\n", PC, M);
+                break;
+            case 2:
+                printf("==> addr: %d     RTN   %d\n", PC, M);
+                break;
+            case 3:
+                printf("==> addr: %d     CAL   %d\n", PC, M);
+                break;
+            case 4:
+                printf("==> addr: %d     POP   %d\n", PC, M);
+                break;
+            case 5:
+                printf("==> addr: %d     PSI   %d\n", PC, M);
+                break;
+            case 6:
+                printf("==> addr: %d     PRM   %d\n", PC, M);
+                break;
+            case 7:
+                printf("==> addr: %d     STO   %d\n", PC, M);
+                break;
+            case 8:
+                printf("==> addr: %d     INC   %d\n", PC, M);
+                break;
+            case 9:
+                printf("==> addr: %d     JMP   %d\n", PC, M);
+                break;
+            case 10:
+                printf("==> addr: %d     JPC   %d\n", PC, M);
+                break;
+            case 11:
+                printf("==> addr: %d     CHO   %d\n", PC, M);
+                break;
+            case 12:
+                printf("==> addr: %d     CHI   %d\n", PC, M);
+                break;
+            case 13:
+                printf("==> addr: %d     HLT   %d\n", PC, M);
+                break;
+            case 14:
+                printf("==> addr: %d     NDB   %d\n", PC, M);
+                printf("N\n");
+                break;
+            case 15:
+                printf("==> addr: %d     NEG   %d\n", PC, M);
+                break;
+            case 16:
+                printf("==> addr: %d     ADD   %d\n", PC, M);
+                break;
+            case 17:
+                printf("==> addr: %d     SUB   %d\n", PC, M);
+                break;
+            case 18:
+                printf("==> addr: %d     MUL   %d\n", PC, M);
+                break;
+            case 19:
+                printf("==> addr: %d     DIV   %d\n", PC, M);
+                break;
+            case 20:
+                printf("==> addr: %d     MOD   %d\n", PC, M);
+                break;
+            case 21:
+                printf("==> addr: %d     EQL   %d\n", PC, M);
+                break;
+            case 22:
+                printf("==> addr: %d     NEQ   %d\n", PC, M);
+                break;
+            case 23:
+                printf("==> addr: %d     LSS   %d\n", PC, M);
+                break;
+            case 24:
+                printf("==> addr: %d     LEQ   %d\n", PC, M);
+                break;
+            case 25:
+                printf("==> addr: %d     GTR   %d\n", PC, M);
+                break;
+            case 26:
+                printf("==> addr: %d     GEQ   %d\n", PC, M);
+                break;
+            case 27:
+                printf("==> addr: %d     PSP   %d\n", PC, M);
+                break;
+        }
+    }
 }
